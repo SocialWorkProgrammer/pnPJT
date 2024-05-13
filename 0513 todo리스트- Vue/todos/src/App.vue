c22859e5b12e28f7e04eace7cda064bcd59c4183
민호 <minho2633@naver.com>
2024-05-13 17:35:35 +0900

    프앤

---
+++ 0513 todo리스트- Vue/todos/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import TodoView from './views/TodoView.vue'
+</script>
+
+<template>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ 0513 todo리스트- Vue/todos/src/components/Todos.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
