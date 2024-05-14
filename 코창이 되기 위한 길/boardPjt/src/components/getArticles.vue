35d1c78c0fab0bbd2502e0b3612e4524e709e339
민호 <minho2633@naver.com>
2024-05-14 17:03:17 +0900

    게시판 구현 중...

---
+++ 코창이 되기 위한 길/boardPjt/src/components/getArticles.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>게시판.</h1>
+        <h2>주의 : 분탕치다 걸리면 영구차단임</h2>
+        <getArticleItem 
+        v-for="article in store.articles"
+        :key="article.id"
+        :article="article" />
+    </div>
+</template>
+
+<script setup>
+import { useBoardStore } from '@/stores/counter'
+import getArticleItem from './getArticleItem.vue'
+const store = useBoardStore()
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
