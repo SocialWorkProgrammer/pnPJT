e35e9a7f71c9a2dc21743e673218dd7dbfb0d752
민호 <minho2633@naver.com>
2024-05-20 20:55:03 +0900

    null ParentNode 오류 해결(사실 뭐가 문제인지 모름)

--- boardPjt/src/router/index.js
+++ boardPjt/src/router/index.js
@@ -7,9 +7,6 @@ import LoginView from '@/views/HomeView/LoginView.vue'
 import SignUpView from '@/views/HomeView/SignUpView.vue'
 import MapView from '@/views/FunctionView/MapView.vue'
 import ExchangeView from '@/views/FunctionView/ExchangeView.vue'
-import ArticleUpdate from '@/components/articles/ArticleUpdate.vue'
-import CommentUpdate from '@/components/comments/CommentUpdate.vue'
-import { useBoardStore } from '@/stores/counter'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,48 +14,44 @@ const router = createRouter({
     {
       // 메인페이지
       path: '/',
-      name: 'HomeView',
-      component: HomeView,
       children: [
-        { path: 'login', name: 'LoginView', component: LoginView },
-        { path: 'signup', name: 'SignUpView', component: SignUpView },
-        { path: 'exchange', name: 'ExchangeView', component: ExchangeView }
+        {path: '', name: 'HomeView', component: HomeView},
+        {path: 'login/', name: 'LoginView', component: LoginView},
+        {path: 'signup/', name: 'SignUpView', component: SignUpView},
+        {path: 'exchange/', name: 'ExchangeView', component: ExchangeView},
       ]
     },
     {
       // 게시판
       path: '/community',
       children: [
-        { path: '', name: 'ArticleView', component: ArticleView },
-        { path: 'create', name: 'CreateArticleView', component: CreateArticleView },
-        { path: 'article/:id', name: 'DetailView', component: DetailView },
-        { path: 'article/:id/update', name: 'ArticleUpdate', component: ArticleUpdate },
-        { path: 'article/:id/comment/:commentId', name: 'CommentUpdate', component: CommentUpdate, props: true },
+        {path: '', name: 'ArticleView', component: ArticleView},
+        {path : 'create/', name: "CreateArticleView", component: CreateArticleView},
+        {path : 'article/:id/', name: "DetailView", component: DetailView},
       ]
     },
     {
       // 지도
       path: '/map',
-      name: 'MapView',
-      component: MapView
-    }
+      children: [
+        {path: '', name: 'MapView', component: MapView},
+      ]
+    },
   ]
 })
 
-router.beforeEach((to, from, next) => {
-  const store = useBoardStore()
-  
-  // 로그인 필수 페이지 체크
-  const requiresAuth = ['ArticleView', 'CreateArticleView', 'DetailView', 'ArticleUpdate', 'CommentUpdate']
-  if (requiresAuth.includes(to.name) && !store.isLogin) {
-    window.alert('로그인해주세요')
-    next({ name: 'LoginView' })
-  } else if ((to.name === 'SignUpView' || to.name === 'LoginView') && store.isLogin) {
-    window.alert('이미 로그인이 된 상태입니다.')
-    next({ name: 'HomeView' })
-  } else {
-    next()
-  }
-})
+// import { useBoardStore } from '@/stores/counter'
+
+// router.beforeEach((to, from) => {
+//   const store = useBoardStore()
+//   if (to.name === 'Articleview' && store.isLogin === false) {
+//     window.alert('로그인해주세요')
+//     return { name : 'LoginView'}
+//   }
+//   if ((to.name === 'SignUpView' || to.name === 'LoginView') && (store.isLogin))
+//   {window.alert('이미 로그인이 된 상태입니다.')
+//   return { name: 'HomeView'}
+// }
+// })
 
 export default router
