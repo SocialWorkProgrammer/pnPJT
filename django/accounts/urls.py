a393749e79f7873e3ddf572a02ad852c78e6dac9
민호 <minho2633@naver.com>
2024-05-24 15:55:23 +0900

    최종 올리기 위해 잠깐 지움

--- django/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='profile'
-urlpatterns = [
-    path('<str:username>/', views.user_profile),
-    path('<str:username>/detail/', views.user_detail),
-    # path('<str:username>/detail/histogram', views.deposit_product_histogram),
-] 
